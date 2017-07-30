a7896e8fe6339b65b5228a3962e43c9510dc073d
Nishant Yadav <tonishantrao@gmail.com>
2017-07-30 22:14:18 +0530

    Location Service Implemented

--- PokeFinder/ViewController.swift
+++ PokeFinder/ViewController.swift
@@ -7,19 +7,107 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    var mapHasCenteredOnce = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
+        
+        // Set user tracking mode to follow.
+        mapView.userTrackingMode = MKUserTrackingMode.follow
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        locationAuthStatus()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func spotRandomPokemon(_ sender: Any) {
+        
+        
+    }
+    
+    func locationAuthStatus() {
+        
+        // Authorize location access only when app is running.
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            
+            mapView.showsUserLocation = true
+        } else {
+            
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    // Called when user gives authorization to access location (or not).
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status == .authorizedWhenInUse {
+            
+            mapView.showsUserLocation = true
+        }
+    }
+    
+    // To zoom map on users current location.
+    func centerMapOnLocation(location: CLLocation) {
+        
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 2000, 2000)
+        
+        mapView.setRegion(coordinateRegion, animated: true)
     }
 
-
+    // When user location is updated.
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        
+        if let loc = userLocation.location {
+            
+            if !mapHasCenteredOnce {
+                
+                centerMapOnLocation(location: loc)
+                mapHasCenteredOnce = true
+                
+            }
+        }
+    }
+    
+    // Change user location blue circle to custom image.
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        var annotationView: MKAnnotationView?
+        
+        if annotation.isKind(of: MKUserLocation.self) {
+            
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
+            annotationView?.image = UIImage(named: "ash")
+        }
+        
+        return annotationView
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
