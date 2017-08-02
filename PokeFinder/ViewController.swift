0ed653449b3ca4e2243657e759677b31a631224a
Nishant Yadav <tonishantrao@gmail.com>
2017-08-02 14:04:56 +0530

    Spot random Pokemon and Navigate using Apple Maps.

--- PokeFinder/ViewController.swift
+++ PokeFinder/ViewController.swift
@@ -90,14 +90,39 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     }
     
     // Change user location blue circle to custom image.
+    // This function will be called whenever we call "addAnnotation".
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         
+        let annoIdentifier = "Pokemon"
         var annotationView: MKAnnotationView?
         
         if annotation.isKind(of: MKUserLocation.self) {
             
             annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
             annotationView?.image = UIImage(named: "ash")
+            
+        } else if let deqAnno = mapView.dequeueReusableAnnotationView(withIdentifier: annoIdentifier) {
+            
+            annotationView = deqAnno
+            annotationView?.annotation = annotation
+            
+        } else {
+            
+            let av = MKAnnotationView(annotation: annotation, reuseIdentifier: annoIdentifier)
+            av.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            annotationView = av
+            
+        }
+        
+        if let annotationView = annotationView, let anno = annotation as? PokeAnnotation {
+            
+            annotationView.canShowCallout = true
+            annotationView.image = UIImage(named: "\(anno.pokemonNumber)")
+            let btn = UIButton()
+            btn.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+            btn.setImage(UIImage(named: "map"), for: .normal)
+            annotationView.rightCalloutAccessoryView = btn
+            
         }
         
         return annotationView
@@ -109,7 +134,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         geoFire.setLocation(location, forKey: "\(pokeId)")
     }
     
-    // Show sightings of Pokemon on map.
+    // Show sightings of Pokemon on map. Refer to GeoFire Documantation.
     func showSightings(location: CLLocation) {
         
         let circleQuery = geoFire!.query(at: location, withRadius: 2.5)
@@ -123,6 +148,32 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
                 
             }
         })
+    }
+    
+    // When region changes beyond 2.5 kms.
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        
+        let loc = CLLocation(latitude: mapView.centerCoordinate.latitude, longitude: mapView.centerCoordinate.longitude)
+        showSightings(location: loc)
+        
+    }
+    
+    // When a Pokemon button is tapped.
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+        if let anno = view.annotation as? PokeAnnotation {
+            
+            let place = MKPlacemark(coordinate: anno.coordinate)
+            let destination = MKMapItem(placemark: place)
+            destination.name = "Pokemon Sighting"
+            let regionDistance: CLLocationDistance = 1000
+            let regionSpan = MKCoordinateRegionMakeWithDistance(anno.coordinate, regionDistance, regionDistance)
+            
+            let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center), MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span), MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving] as [String : Any]
+            
+            MKMapItem.openMaps(with: [destination], launchOptions: options)
+        }
+        
     }
     
 }
